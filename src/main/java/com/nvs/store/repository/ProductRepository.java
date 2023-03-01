9657dda9088d287cadc0aae4a51bb6a5852b6f85
azabolotnaiaGD <azabolotnaia@griddynamics.com>
2023-03-01 14:45:07 +0200

    Revert "small refactor in ProductController , Service and Repo"

--- src/main/java/com/nvs/store/repository/ProductRepository.java
+++ src/main/java/com/nvs/store/repository/ProductRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product getProductById(Long id);
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findAll();
 
-    Product  findTopByOrderByIdDesc();
+    List<Product> findAllByIdIsLike(Long id);
+
+    Product getProductById(Long id);
 
 }
